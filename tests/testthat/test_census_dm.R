7ddd93c3519e604d7bc10f8402d73b14cf7bbabe
Joli Holmes <jh111@rice.edu>
2021-03-19 15:49:37 -0500

    added tests to dwnld.acs

--- tests/testthat/test_census_dm.R
+++ tests/testthat/test_census_dm.R
@@ -1,21 +1,21 @@
 testthat::test_that('Test test_attr fails correctly', {
-  
+
   attr <- list()
   testthat::expect_error(test_attr(attr))
-  
+
   attr <- list(variables = "blah",
                geography = "blah")
   testthat::expect_true(test_attr(attr))
-  
+
   attr <- list(table = "blah",
                geography = "blah")
   testthat::expect_true(test_attr(attr))
-  
+
   attr <- list(table = "blah",
                variables = "blah_01",
                geography = "blah")
   testthat::expect_true(test_attr(attr))
-  
+
   attr <- list(table = "blah",
                variables = "not_blah_01",
                geography = "blah")
@@ -23,9 +23,10 @@ testthat::test_that('Test test_attr fails correctly', {
 })
 
 testthat::test_that('Test that ACS tables download correctly', {
-  
-  dwnld.acs(tbls,
-            pth)
-  
-  
+
+  tbls <- list(B23008 = list(year = 2019, state = 48,
+                             geography = "tract", county = 439))
+
+  testthat::expect_true(all(unlist(dwnld.acs(tbls,
+                                             pth = NULL))))
 })
