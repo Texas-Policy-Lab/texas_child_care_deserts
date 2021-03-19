985bcce1a09e3e105f5f81a884e98d333ea3b8b5
Joli Holmes <jh111@rice.edu>
2021-03-19 15:49:03 -0500

    updated to just vectorize path creation

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,14 +1,12 @@
 #' @title Create data folder structure
 #' @param root string. The root directory
 #' @param pth list. A list of paths to create in the root directory
-create_folder_str <- function(root, pths) {
+create_folder_str <- function(pths) {
 
-  lapply(pths, function(p, root) {
-    
-    pth <- file.path(root, p)
-    
+  lapply(pths, function(pth) {
+
     if(!dir.exists(pth)) {
       dir.create(pth, recursive = TRUE)
     }
-  }, root = root)
+  })
 }
