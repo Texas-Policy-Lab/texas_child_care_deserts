f24bc2e3f5968af31be0d9836e4ba11319652499
Joli Holmes <jh111@rice.edu>
2021-03-23 12:40:35 -0500

    updated documentation

--- R/input_cols.R
+++ R/input_cols.R
@@ -5,9 +5,9 @@
 #' @param input_columns. Vector. Vector of variable names to select
 #' @return data.frame
 test_input <- function(df, input_columns) {
-  
+
   assertthat::assert_that(all(c(input_columns %in% colnames(df))))
-  
+
   df %>%
     dplyr::select(dplyr::one_of(input_columns))
 }
@@ -16,32 +16,34 @@ test_input <- function(df, input_columns) {
 #' @param v. Vector. A vector to test.
 test_type <- function(v) UseMethod("test_type")
 
-#' @title Test for character column
+#' @title Test for character (string) column or vector
+#' @inheritParams test_type
 test_type.character <- function(v) {
 
   assertthat::assert_that(typeof(v) == "character" & class(v) == "character",
                           msg = "Not a character (string) vector")
 }
 
-#' @title Test for numeric column
+#' @title Test for numeric column or vector
+#' @inheritParams test_type
 test_type.numeric <- function(v) {
 
   assertthat::assert_that(typeof(v) == "double" & class(v) != "Date",
                           msg = "Not a numeric vector")
 }
 
-#' @title Test for date column
+#' @title Test for date column or vector
+#' @inheritParams test_type
 test_type.date <- function(v) {
-  
+
   assertthat::assert_that(typeof(v) == "double" & class(v) == "Date",
                           msg = "Not a date vector")
 }
 
-#' @title Test for date column
+#' @title Test for boolean (logical) column or vector
+#' @inheritParams test_type
 test_type.boolean <- function(v) {
   
   assertthat::assert_that(typeof(v) == "logical" & class(v) == "logical",
                           msg = "Not a boolean (logical) vector")
 }
-
-
