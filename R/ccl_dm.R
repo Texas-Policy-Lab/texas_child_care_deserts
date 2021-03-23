a202d4f1953fc0d12e6b3769c125419f8494b135
Joli Holmes <jh111@rice.edu>
2021-03-23 10:26:17 -0500

    made the string variables lower case to make the function more robust of the case changes

--- R/ccl_dm.R
+++ R/ccl_dm.R
@@ -8,13 +8,13 @@ get.hhsc_ccl_data <- function(data_in_name,
                               data_in_pth,
                               url = "https://data.texas.gov/api/views/bc5r-88dy/rows.csv?accessType=DOWNLOAD",
                               download_date = Sys.Date()) {
-  
+
   fl_name <- glue::glue(as.character(download_date), data_in_name, .sep = "_")
-  
+
   dwnld_pth <- file.path(data_in_pth, fl_name)
-  
+
   download.file(url, destfile = dwnld_pth, mode = "wb")
-  
+
 }
 
 #' @title HHSC CCL data management
@@ -24,35 +24,34 @@ get.hhsc_ccl_data <- function(data_in_name,
 #' @param ccl_data_out_pth string. The path to write the cleaned ccl data to.
 #' @param ccl_data_out_name string. The name of the data to write out. 
 #' @param county_name string. The name of the county to subset to. 
-
 dm.ccl <- function(ccl_data_in_pth,
                    ccl_data_in_name,
                    ccl_data_out_pth,
                    ccl_data_out_name,
                    county_name) {
-  
+
   df <- read.csv(file.path(ccl_data_in_pth, ccl_data_in_name), stringsAsFactors = F)
-  
+
   df <- df %>% 
     dplyr::rename_all(tolower) %>% 
     tidyr::separate(location_address_geo, 
                     into = c("address", "lat", "long"),
                     sep = "([(,)])")
-  
+
   assertthat::assert_that(all(c("operation_number", "licensed_to_serve_ages", "operation_type", 
                                 "programs_provided", "operation_name", "accepts_child_care_subsidies") %in% names(df)),
                           msg = "CCL data frame missing needed variables")
-  
-  df <- df %>% 
+
+  df <- df %>%
     dplyr::mutate(operation_number = gsub("-.*", "", operation_number),
-                  infant = ifelse(grepl("Infant", licensed_to_serve_ages), 1, 0),
-                  toddler = ifelse(grepl("Toddler", licensed_to_serve_ages), 1, 0),
-                  prek = ifelse(grepl("Pre-Kindergarten", licensed_to_serve_ages), 1, 0),
-                  school = ifelse(grepl("School", licensed_to_serve_ages), 1, 0),
-                  home_prvdr = ifelse(grepl("Home", operation_type), 1, 0),
-                  center_prvdr = ifelse(grepl("Center", operation_type), 1, 0),
-                  after_school = ifelse(grepl("After School Care", programs_provided), 1, 0),
-                  head_start = ifelse(grepl("Head Start", operation_name), 1, 0),
+                  infant = ifelse(grepl("infant", tolower(licensed_to_serve_ages)), 1, 0),
+                  toddler = ifelse(grepl("toddler", tolower(licensed_to_serve_ages)), 1, 0),
+                  prek = ifelse(grepl("pre-kindergarten", tolower(licensed_to_serve_ages)), 1, 0),
+                  school = ifelse(grepl("school", tolower(licensed_to_serve_ages)), 1, 0),
+                  home_prvdr = ifelse(grepl("home", tolower(operation_type)), 1, 0),
+                  center_prvdr = ifelse(grepl("center", tolower(operation_type)), 1, 0),
+                  after_school = ifelse(grepl("after school care", tolower(programs_provided)), 1, 0),
+                  head_start = ifelse(grepl("head start", tolower(operation_name)), 1, 0),
                   subsidy = ifelse(accepts_child_care_subsidies == "Y", 1, 0),
                   download_date = as.Date(gsub("_.*", "", ccl_data_in_name))) %>% 
     dplyr::select(operation_number,
@@ -74,17 +73,17 @@ dm.ccl <- function(ccl_data_in_pth,
                   phone_number,
                   email_address,
                   download_date) 
-  
+
   if(!is.null(county_name)) {
-    
+  
     df <- df %>% 
       dplyr::filter(county == county_name)
-    
+
     assertthat::assert_that(all(df$county == county_name))
   }
-  
+
   write.csv(df, file.path(ccl_data_out_pth, ccl_data_out_name), row.names = FALSE)
-  
+
   return(df)
 }
 
