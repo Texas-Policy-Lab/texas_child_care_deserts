87ebcc64d3d2dec0f71ac0b15cd5c5a62fa6a589
Joli Holmes <jh111@rice.edu>
2021-03-23 10:35:11 -0500

    simplified the variable names

--- R/ccl_dm.R
+++ R/ccl_dm.R
@@ -1,17 +1,17 @@
 #' @title Get HHSC CCL data
 #' @description Returns the most recent HHSC CCL Daycare and Residential Operations 
 #' Data. Link to data: https://data.texas.gov/resource/bc5r-88dy.csv
-#' @param data_in_name string. The name to of the data to read in.
-#' @param data_in_pth string. The path to read the data in from.
+#' @param name string. The name to write the raw data to.
+#' @param pth string. The path to write the raw to.
 #' @export
-get.hhsc_ccl_data <- function(data_in_name,
-                              data_in_pth,
+get.hhsc_ccl_data <- function(name,
+                              pth,
                               url = "https://data.texas.gov/api/views/bc5r-88dy/rows.csv?accessType=DOWNLOAD",
                               download_date = Sys.Date()) {
 
-  fl_name <- glue::glue(as.character(download_date), data_in_name, .sep = "_")
+  fl_name <- glue::glue(as.character(download_date), name, .sep = "_")
 
-  dwnld_pth <- file.path(data_in_pth, fl_name)
+  dwnld_pth <- file.path(pth, fl_name)
 
   download.file(url, destfile = dwnld_pth, mode = "wb")
 
