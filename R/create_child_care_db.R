942b90c983a6cd12a2adbca37bf933a130848c9c
Joli Holmes <jh111@rice.edu>
2021-03-23 13:52:16 -0500

    updated parameter names

--- R/create_child_care_db.R
+++ R/create_child_care_db.R
@@ -1,6 +1,8 @@
 #' @title Create child care data base
-#' @param root string. Path to the root directory to create the DB
-#' @param census_tbls. List of census tables to pull
+#' @param census_tbls_config. Configuration for the list of census tables to 
+#' pull.
+#' @param hhsc_ccl_config. Configuration for the CCL_HHS to pull.
+#' @param root string. Path to the root directory to create the DB.
 #' @examples
 #' \dontrun{
 #' census_tbls <- list(B23008 = list(year = 2019, state = 48, 
@@ -8,18 +10,23 @@
 #' root <- "C:/"
 #' childcare_db(census_tbls = census_tbls, root = root)
 #' }
-childcare_db <- function(census_tbls,
+childcare_db <- function(census_tbls_config,
+                         hhsc_ccl_config,
                          root) {
 
-  pth_data <- file.path(root, "data")
-  pth_raw <- file.path(pth_data, "raw")
-  pth_processed <- file.path(pth_data, "processed")
+  data_pth <- file.path(root, "data")
+  raw_pth <- file.path(data_pth, "raw")
+  processed_pth <- file.path(data_pth, "processed")
 
-  pths <- c(pth_data, pth_raw, pth_processed)
+  pths <- c(data_pth, raw_pth, processed_pth)
 
   create_folder_str(pths = pths)
 
   dwnld.acs(tbls = census_tbls,
-            pth = pth_raw)
+            pth = raw_pth)
+
+  process.hhsc_ccl(hhsc_ccl = hhsc_ccl_config,
+                   raw_pth = raw_pth,
+                   processed_pth = processed_pth)
 
 }
