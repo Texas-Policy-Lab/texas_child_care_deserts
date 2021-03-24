d3ede36089f3c67bffaebc1563aa2eac3bdc7f90
Joli Holmes <jh111@rice.edu>
2021-03-24 13:19:41 -0500

    updated to match function updates

--- tests/testthat/test_ccl_dm.R
+++ tests/testthat/test_ccl_dm.R
@@ -23,37 +23,20 @@ testthat::test_that('Test county column conversion function', {
 
   df <- data.frame(county = c("TARRANT", "HARRIS", "EL PASO", "TOM GREEN"))
 
-  # Test that we can pass in either a character or numeric FIPS code
-  testthat::expect_is(col.county(df, county = "48439"), "data.frame")
-  testthat::expect_is(col.county(df, county = 48439), "data.frame")
-  testthat::expect_is(col.county(df, county = NULL), "data.frame")
+  testthat::expect_is(col.county(df), "data.frame")
 
   # Test that the number of rows returned is equal to 1
-  testthat::expect_equal(nrow(col.county(df, county = "48439")), 1)
-  testthat::expect_equal(nrow(col.county(df, county = 48439)), 1)
-  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
+  testthat::expect_gte(nrow(col.county(df)), 4)
 
-  # Test that passing a fake FIPS code will return 0 rows
-  testthat::expect_equal(nrow(col.county(df, county = "48438")), 0)
-  testthat::expect_equal(nrow(col.county(df, county = 48438)), 0)
-  
   # Test that passing the county name in a different format will still return the
   # intended result
   df <- data.frame(county = c("tarrant", "harris", "el paso", "tom green"))
-  testthat::expect_equal(nrow(col.county(df, county = "48439")), 1)
-  testthat::expect_equal(nrow(col.county(df, county = 48439)), 1)
-  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
+  testthat::expect_gte(nrow(col.county(df)), 4)
   
   # Test that passing the county name in a different format will still return the
   # intended result
   df <- data.frame(county = c("tarrant", "harris ", "el paso", "tom green"))
-  testthat::expect_equal(nrow(col.county(df, county = "48201")), 1)
-  testthat::expect_equal(nrow(col.county(df, county = 48201)), 1)
-  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
-
-  # Test that passing county as a name will fail
-  testthat::expect_error(col.county(df, county = "tarrant"))
-  testthat::expect_error(col.county(df, county = "Tarrant"))
+  testthat::expect_gte(nrow(col.county(df)), 4)
 
 })
 
@@ -79,4 +62,4 @@ testthat::test_that('Test data management col.total_capacity', {
   df <- data.frame(total_capacity = c(1,2,3,4, NA))
   testthat::expect_true(all(is.numeric(col.total_capacity(df)$licensed_capacity)))
 
-}
+})
