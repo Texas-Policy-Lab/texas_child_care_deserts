08ddd06ddea50c182f8e889d6b22a3c8ba94e7e8
Joli Holmes <jh111@rice.edu>
2021-03-24 13:19:27 -0500

    updated function name to distinguish between formalized tests

--- R/input_cols.R
+++ R/input_cols.R
@@ -14,7 +14,7 @@ test_input <- function(df, input_columns) {
     cls <- input_columns[[col]]
     test_col <- structure(list(v = df[[col]]), 
                           class = cls)
-    do.call(test_type, test_col)
+    do.call(check_type, test_col)
     })
   ))
 
@@ -24,35 +24,35 @@ test_input <- function(df, input_columns) {
 
 #' @title Tests the vector or column type
 #' @param v. Vector. A vector to test.
-test_type <- function(v) UseMethod("test_type")
+check_type <- function(v) UseMethod("check_type")
 
 #' @title Test for character (string) column or vector
-#' @inheritParams test_type
-test_type.character <- function(v) {
+#' @inheritParams check_type
+check_type.character <- function(v) {
 
   assertthat::assert_that(class(v) == "character",
                           msg = "Not a character (string) vector")
 }
 
 #' @title Test for numeric column or vector
-#' @inheritParams test_type
-test_type.numeric <- function(v) {
+#' @inheritParams check_type
+check_type.numeric <- function(v) {
 
   assertthat::assert_that(class(v) == "numeric",
                           msg = "Not a numeric vector")
 }
 
 #' @title Test for date column or vector
-#' @inheritParams test_type
-test_type.date <- function(v) {
+#' @inheritParams check_type
+check_type.date <- function(v) {
 
   assertthat::assert_that(class(v) == "Date",
                           msg = "Not a date vector")
 }
 
 #' @title Test for boolean (logical) column or vector
-#' @inheritParams test_type
-test_type.boolean <- function(v) {
+#' @inheritParams check_type
+check_type.boolean <- function(v) {
   
   assertthat::assert_that(class(v) == "logical",
                           msg = "Not a boolean (logical) vector")
--- tests/testthat/test_input_cols.R
+++ tests/testthat/test_input_cols.R
@@ -1,24 +1,24 @@
 testthat::test_that('Tests character column function', {
 
   # Test character values
-  testthat::expect_true(test_type.character(v = "dsfsdlkj"))
-  testthat::expect_true(test_type.character(v = c("sf", "dsfsdlkj")))
-  testthat::expect_true(test_type.character(v = c("dfalksj", NA)))
-  testthat::expect_true(test_type.character(v = c("dfalksj", NULL)))
+  testthat::expect_true(check_type.character(v = "dsfsdlkj"))
+  testthat::expect_true(check_type.character(v = c("sf", "dsfsdlkj")))
+  testthat::expect_true(check_type.character(v = c("dfalksj", NA)))
+  testthat::expect_true(check_type.character(v = c("dfalksj", NULL)))
 
   # Test numeric values
-  testthat::expect_error(test_type.character(v = c(1,2,4)))
-  testthat::expect_error(test_type.character(v = 1))
-  testthat::expect_error(test_type.character(v = c(1, NA)))
-  testthat::expect_error(test_type.character(v = c(1.1, 1.3)))
+  testthat::expect_error(check_type.character(v = c(1,2,4)))
+  testthat::expect_error(check_type.character(v = 1))
+  testthat::expect_error(check_type.character(v = c(1, NA)))
+  testthat::expect_error(check_type.character(v = c(1.1, 1.3)))
 
   # Test Boolean values
-  testthat::expect_error(test_type.character(v = c(TRUE, FALSE)))
-  testthat::expect_error(test_type.character(v = TRUE))
+  testthat::expect_error(check_type.character(v = c(TRUE, FALSE)))
+  testthat::expect_error(check_type.character(v = TRUE))
 
   # Test Date values
-  testthat::expect_error(test_type.character(v = as.Date("2021-03-23")))
-  testthat::expect_error(test_type.character(v = c(as.Date("2021-03-22"),
+  testthat::expect_error(check_type.character(v = as.Date("2021-03-23")))
+  testthat::expect_error(check_type.character(v = c(as.Date("2021-03-22"),
                                                    as.Date("2021-03-23"))))
 
 })
@@ -26,24 +26,24 @@ testthat::test_that('Tests character column function', {
 testthat::test_that('Tests numeric column function', {
 
   # Test character values
-  testthat::expect_error(test_type.numeric(v = "dsfsdlkj"))
-  testthat::expect_error(test_type.numeric(v = c("sf", "dsfsdlkj")))
-  testthat::expect_error(test_type.numeric(v = c("dfalksj", NA)))
-  testthat::expect_error(test_type.numeric(v = c("dfalksj", NULL)))
+  testthat::expect_error(check_type.numeric(v = "dsfsdlkj"))
+  testthat::expect_error(check_type.numeric(v = c("sf", "dsfsdlkj")))
+  testthat::expect_error(check_type.numeric(v = c("dfalksj", NA)))
+  testthat::expect_error(check_type.numeric(v = c("dfalksj", NULL)))
 
   # Test numeric values
-  testthat::expect_true(test_type.numeric(v = c(1,2,4)))
-  testthat::expect_true(test_type.numeric(v = 1))
-  testthat::expect_true(test_type.numeric(v = c(1, NA)))
-  testthat::expect_true(test_type.numeric(v = c(1.1, 1.3)))
+  testthat::expect_true(check_type.numeric(v = c(1,2,4)))
+  testthat::expect_true(check_type.numeric(v = 1))
+  testthat::expect_true(check_type.numeric(v = c(1, NA)))
+  testthat::expect_true(check_type.numeric(v = c(1.1, 1.3)))
 
   # Test Boolean values
-  testthat::expect_error(test_type.numeric(v = c(TRUE, FALSE)))
-  testthat::expect_error(test_type.numeric(v = TRUE))
+  testthat::expect_error(check_type.numeric(v = c(TRUE, FALSE)))
+  testthat::expect_error(check_type.numeric(v = TRUE))
 
   # Test Date values
-  testthat::expect_error(test_type.numeric(v = as.Date("2021-03-23")))
-  testthat::expect_error(test_type.numeric(v = c(as.Date("2021-03-22"),
+  testthat::expect_error(check_type.numeric(v = as.Date("2021-03-23")))
+  testthat::expect_error(check_type.numeric(v = c(as.Date("2021-03-22"),
                                                  as.Date("2021-03-23"))))
 
 })
@@ -51,24 +51,24 @@ testthat::test_that('Tests numeric column function', {
 testthat::test_that('Tests date column function', {
   
   # Test character values
-  testthat::expect_error(test_type.date(v = "dsfsdlkj"))
-  testthat::expect_error(test_type.date(v = c("sf", "dsfsdlkj")))
-  testthat::expect_error(test_type.date(v = c("dfalksj", NA)))
-  testthat::expect_error(test_type.date(v = c("dfalksj", NULL)))
+  testthat::expect_error(check_type.date(v = "dsfsdlkj"))
+  testthat::expect_error(check_type.date(v = c("sf", "dsfsdlkj")))
+  testthat::expect_error(check_type.date(v = c("dfalksj", NA)))
+  testthat::expect_error(check_type.date(v = c("dfalksj", NULL)))
   
   # Test numeric values
-  testthat::expect_error(test_type.date(v = c(1,2,4)))
-  testthat::expect_error(test_type.date(v = 1))
-  testthat::expect_error(test_type.date(v = c(1, NA)))
-  testthat::expect_error(test_type.date(v = c(1.1, 1.3)))
+  testthat::expect_error(check_type.date(v = c(1,2,4)))
+  testthat::expect_error(check_type.date(v = 1))
+  testthat::expect_error(check_type.date(v = c(1, NA)))
+  testthat::expect_error(check_type.date(v = c(1.1, 1.3)))
   
   # Test Boolean values
-  testthat::expect_error(test_type.date(v = c(TRUE, FALSE)))
-  testthat::expect_error(test_type.date(v = TRUE))
+  testthat::expect_error(check_type.date(v = c(TRUE, FALSE)))
+  testthat::expect_error(check_type.date(v = TRUE))
   
   # Test Date values
-  testthat::expect_true(test_type.date(v = as.Date("2021-03-23")))
-  testthat::expect_true(test_type.date(v = c(as.Date("2021-03-22"),
+  testthat::expect_true(check_type.date(v = as.Date("2021-03-23")))
+  testthat::expect_true(check_type.date(v = c(as.Date("2021-03-22"),
                                                  as.Date("2021-03-23"))))
   
 })
@@ -76,24 +76,24 @@ testthat::test_that('Tests date column function', {
 testthat::test_that('Tests date column function', {
   
   # Test character values
-  testthat::expect_error(test_type.boolean(v = "dsfsdlkj"))
-  testthat::expect_error(test_type.boolean(v = c("sf", "dsfsdlkj")))
-  testthat::expect_error(test_type.boolean(v = c("dfalksj", NA)))
-  testthat::expect_error(test_type.boolean(v = c("dfalksj", NULL)))
+  testthat::expect_error(check_type.boolean(v = "dsfsdlkj"))
+  testthat::expect_error(check_type.boolean(v = c("sf", "dsfsdlkj")))
+  testthat::expect_error(check_type.boolean(v = c("dfalksj", NA)))
+  testthat::expect_error(check_type.boolean(v = c("dfalksj", NULL)))
   
   # Test numeric values
-  testthat::expect_error(test_type.boolean(v = c(1,2,4)))
-  testthat::expect_error(test_type.boolean(v = 1))
-  testthat::expect_error(test_type.boolean(v = c(1, NA)))
-  testthat::expect_error(test_type.boolean(v = c(1.1, 1.3)))
+  testthat::expect_error(check_type.boolean(v = c(1,2,4)))
+  testthat::expect_error(check_type.boolean(v = 1))
+  testthat::expect_error(check_type.boolean(v = c(1, NA)))
+  testthat::expect_error(check_type.boolean(v = c(1.1, 1.3)))
   
   # Test Boolean values
-  testthat::expect_true(test_type.boolean(v = c(TRUE, FALSE)))
-  testthat::expect_true(test_type.boolean(v = TRUE))
+  testthat::expect_true(check_type.boolean(v = c(TRUE, FALSE)))
+  testthat::expect_true(check_type.boolean(v = TRUE))
   
   # Test Date values
-  testthat::expect_error(test_type.boolean(v = as.Date("2021-03-23")))
-  testthat::expect_error(test_type.boolean(v = c(as.Date("2021-03-22"),
+  testthat::expect_error(check_type.boolean(v = as.Date("2021-03-23")))
+  testthat::expect_error(check_type.boolean(v = c(as.Date("2021-03-22"),
                                              as.Date("2021-03-23"))))
   
 })
