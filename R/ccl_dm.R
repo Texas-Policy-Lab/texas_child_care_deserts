39200ccd05d9986ca5aba223a9d2172f97db149b
Joli Holmes <jh111@rice.edu>
2021-03-24 09:22:05 -0500

    refactored code into modular pieces to be able to do individual tests on each function

--- R/ccl_dm.R
+++ R/ccl_dm.R
@@ -1,5 +1,6 @@
 #' @title Get HHSC CCL data
-#' @description Returns the most recent HHSC CCL Daycare and Residential Operations 
+#' @description Returns the most recent HHSC CCL Daycare and Residential 
+#' Operations 
 #' Data. Link to data: https://data.texas.gov/resource/bc5r-88dy.csv
 #' @param name string. The name to write the raw data to.
 #' @param raw_pth string. The path to write the raw to.
@@ -18,117 +19,218 @@ dwnld.hhsc_ccl <- function(name,
   return(df)
 }
 
-#' @title Data management steps for the county column
-#' @inherit dm.hhsc_ccl
-#' @return data.frame
-dm.county_col <- function(df, county) {
 
-  if ("county" %in% names(df)) {
-  
+#' @title Data management steps for the operation number column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.operation_number <- function(df, operation_number) {
+
+  df <- df %>%
+    dplyr::mutate(operation_number = gsub("-.*", "", operation_number))
+
+  assertthat::assert_that(all(!grepl("-", df$operation_number)))
+  assertthat::assert_that(length(unique(df$operation_number)) == nrow(df),
+                          msg = "Data frame is not unique on operation number")
+  return(df)
+}
+
+#' @title Data management steps for the county column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.county <- function(df, county_fips) {
+
+  df <- df %>%
+    dplyr::mutate(county = tolower(gsub("[^[:alnum:]]", "", county))) %>% 
+    dplyr::inner_join(tigris::fips_codes %>% 
+                        dplyr::mutate(county = tolower(gsub("[^[:alnum:]]", "", county)),
+                                      county = gsub("county", "", county),
+                                      county_code = paste0(state_code, county_code)) %>% 
+                        dplyr::select(county, county_code))
+
+  if (!is.null(county_fips)) {
+
+    assertthat::assert_that(!is.na(as.numeric(county_fips)),
+                            msg = "Expecting 5-digit county FIPS code.")
     df <- df %>%
-      dplyr::mutate(county = tolower(gsub("[^[:alnum:]]", "", county))) %>% 
-      dplyr::inner_join(tigris::fips_codes %>% 
-                          dplyr::mutate(county = tolower(gsub("[^[:alnum:]]", "", county)),
-                                        county = gsub("county", "", county),
-                                        county_code = paste0(state_code, county_code)) %>% 
-                          dplyr::select(county, county_code)) %>%
-      dplyr::select(-county)
-  
-    if (!is.null(county)) {
-    
-      assertthat::assert_that(!is.na(as.numeric(county)),
-                              msg = "Expecting 5-digit county FIPS code.")
-      df <- df %>%
-        dplyr::filter(county_code == as.character(county))
+      dplyr::filter(county_code == as.character(county_fips))
+
+    msg <- "Number of rows is 0, nothing to return. Is the county FIPS code 
+            correct?"
+
+    assertthat::assert_that(nrow(df) > 0, msg = msg)
 
   } else {
     df <- df
   }
 
-    msg <- "Number of rows is 0, nothing to return. Is the county FIPS code 
-    correct?"
-  
-    assertthat::assert_that(nrow(df) > 0, msg = msg)
+  return(df)
+}
 
+#' @title Data management steps to clean the licensed to serve ages column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.licensed_to_serve_ages <- function(df) {
+
+  df <- df %>%
+    dplyr::mutate(infant = ifelse(grepl("infant",
+                                        tolower(licensed_to_serve_ages)),
+                                  TRUE, FALSE),
+                  toddler = ifelse(grepl("toddler",
+                                         tolower(licensed_to_serve_ages)),
+                                   TRUE, FALSE),
+                  prek = ifelse(grepl("pre-kindergarten",
+                                      tolower(licensed_to_serve_ages)),
+                                TRUE, FALSE),
+                  school = ifelse(grepl("school",
+                                        tolower(licensed_to_serve_ages)),
+                                  TRUE, FALSE),
+    ) %>%
+    dplyr::select(operation_number, infant, toddler, prek, school)
+
+  assertthat::assert_that(all(c(df$infant, df$toddler, df$prek, df$school) 
+                              %in% c(TRUE, FALSE)),
+                          msg = "Licensed to serve age not binary")
+  return(df)
+}
+
+#' @title Data management steps to clean the operation type column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.location_address_geo <- function(df) {
+
+  df <- df %>%
+    tidyr::separate(location_address_geo,
+                    into = c("address", "lat", "long"),
+                    sep = "([(,)])")
+
+  return(df)
+}
+
+#' @title Data management steps to clean the location address geo column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.operation_type <- function(df) {
+
+  df <- df %>% 
+    dplyr::mutate(home_prvdr = ifelse(grepl("home", 
+                                            tolower(operation_type)), TRUE, FALSE),
+                  center_prvdr = ifelse(grepl("center", 
+                                              tolower(operation_type)), TRUE, FALSE)
+    )
+
+  assertthat::assert_that(all(c(df$home_prvdr, df$center_prvdr) %in% c(TRUE, FALSE)),
+                          msg = "Operation type not binary")
+
+  return(df)
+}
+
+#' @title Data management steps to clean the operation name column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.operation_name <- function(df) {
+
+  df <- df %>% 
+    dplyr::mutate(head_start = ifelse(grepl("head start",
+                                            tolower(operation_name)), TRUE, FALSE))
+  
+  assertthat::assert_that(all(c(df$head_start) %in% c(TRUE, FALSE)),
+                          msg = "Operation characteristics not binary")
+  return(df)
+}
+
+#' @title Data management steps to clean the programs provided column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.programs_provided <- function(df) {
+
+  df <- df %>%
+    dplyr::mutate(after_school = ifelse(grepl("after school care", 
+                                              tolower(programs_provided)), TRUE, FALSE))
+
+  assertthat::assert_that(all(c(df$after_school) %in% c(TRUE, FALSE)),
+                          msg = "Operation characteristics not binary")
+  return(df)
+}
+
+#' @title Data management steps to clean the accepts child care subsidies column
+#' @inheritParams dm.hhsc_ccl
+#' @return data.frame
+col.accepts_child_care_subsidies <- function(df) {
+
+  if (all(unique(df$accepts_child_care_subsidies) %in% c("Y", "N")) == F) {
+    print("Additional values besides 'Y' and 'N' in 'Accepts child care subsidies' column")
   }
+
+  df <- df %>%
+    dplyr::mutate(subsidy = dplyr::case_when(accepts_child_care_subsidies == "Y" ~ TRUE,
+                                             accepts_child_care_subsidies == "N" ~ FALSE,
+                                             TRUE ~ NA_real_))
+
+  assertthat::assert_that(all(c(df$subsidy) %in% c(TRUE, FALSE, NA)),
+                          msg = "Operation characteristics not binary")
   return(df)
 }
 
 #' @title HHSC CCL data management
-#' @description Clean CCL download data, convert key variables to binary and select
+#' @description Clean CCL download data, convert key variables to binary and 
+#' select variables
 #' @param df data.frame. The dataframe
 #' @param input_columns. List. List of the columns to keep.
-#' @param county. Integer. The FIPS code for the county.
+#' @param county_fips. Integer. The FIPS code for the county.
 #' @param pth. string. Path to store the processed data.
 #' @param name. string. Name of the data.
 #' @return data.frame
 dm.hhsc_ccl <- function(df,
                         input_columns,
-                        county = NULL,
+                        county_fips = NULL,
                         processed_pth,
                         name,
                         ...) {
 
   df <- df %>%
-    test_input(input_columns)
-
-  df <- df %>%
+    test_input(input_columns) %>%
     dplyr::rename_all(tolower) %>%
-    tidyr::separate(location_address_geo,
-                    into = c("address", "lat", "long"),
-                    sep = "([(,)])")
+    col.operation_number() %>%
+    col.county(county_fips = county_fips) %>%
+    col.location_address_geo() %>%
+    col.licensed_to_serve_ages() %>%
+    col.operation_type() %>%
+    col.operation_name() %>%
+    col.programs_provided() %>%
+    col.accepts_child_care_subsidies()
+    
+    #               after_school = ifelse(grepl("after school care", tolower(programs_provided)), 1, 0),
+    #               subsidy = ifelse(accepts_child_care_subsidies == "Y", 1, 0),
+    #               download_date = Sys.Date()) %>% 
+    # dplyr::select(operation_number,
+    #               operation_name,
+    #               licensed_capacity = total_capacity,
+    #               county,
+    #               address,
+    #               lat,
+    #               long,
+    #               infant,
+    #               toddler,
+    #               prek,
+    #               school,
+    #               home_prvdr,
+    #               center_prvdr,
+    #               after_school,
+    #               head_start,
+    #               subsidy,
+    #               phone_number,
+    #               email_address,
+    #               download_date)
 
-  df <- df %>%
-    dplyr::mutate(operation_number = gsub("-.*", "", operation_number),
-                  infant = ifelse(grepl("infant", tolower(licensed_to_serve_ages)), 1, 0),
-                  toddler = ifelse(grepl("toddler", tolower(licensed_to_serve_ages)), 1, 0),
-                  prek = ifelse(grepl("pre-kindergarten", tolower(licensed_to_serve_ages)), 1, 0),
-                  school = ifelse(grepl("school", tolower(licensed_to_serve_ages)), 1, 0),
-                  home_prvdr = ifelse(grepl("home", tolower(operation_type)), 1, 0),
-                  center_prvdr = ifelse(grepl("center", tolower(operation_type)), 1, 0),
-                  after_school = ifelse(grepl("after school care", tolower(programs_provided)), 1, 0),
-                  head_start = ifelse(grepl("head start", tolower(operation_name)), 1, 0),
-                  subsidy = ifelse(accepts_child_care_subsidies == "Y", 1, 0),
-                  download_date = Sys.Date()) %>% 
-    dplyr::select(operation_number,
-                  operation_name,
-                  licensed_capacity = total_capacity,
-                  county,
-                  address,
-                  lat,
-                  long,
-                  infant,
-                  toddler,
-                  prek,
-                  school,
-                  home_prvdr,
-                  center_prvdr,
-                  after_school,
-                  head_start,
-                  subsidy,
-                  phone_number,
-                  email_address,
-                  download_date) %>% 
-    dm.county_col(county = county)
-
-  assertthat::assert_that(length(unique(df$operation_number)) == nrow(df),
-                          msg = "Data frame is not unique on operation number")
-
-  assertthat::assert_that(is.numeric(df$licensed_capacity),
-                          msg = "Capacity not numeric")
-  
-  assertthat::assert_that(all(c(df$infant, df$toddler, df$prek, df$school) %in% c(1,0)),
-                          msg = "Licensed to serve age not binary")
-  
-  assertthat::assert_that(all(c(df$home_prvdr, df$center_prvdr) %in% c(1,0)),
-                          msg = "Operation type not binary")
-  
-  assertthat::assert_that(all(c(df$after_school, df$head_start, df$subsidy) %in% c(1,0)),
-                          msg = "Operation characteristics not binary")
-
-  readr::write_csv(df, file.path(processed_pth, name))
-
-  return(df)
+#   assertthat::assert_that(is.numeric(df$licensed_capacity),
+#                           msg = "Capacity not numeric")
+# # 
+#   assertthat::assert_that(all(c(df$after_school, df$head_start, df$subsidy) %in% c(1,0)),
+#                           msg = "Operation characteristics not binary")
+# 
+#   readr::write_csv(df, file.path(processed_pth, name))
+# 
+#   return(df)
 }
 
 #' @title Process the CCL data
--- tests/testthat/test_ccl_dm.R
+++ tests/testthat/test_ccl_dm.R
@@ -1,72 +1,37 @@
-testthat::test_that('test ccl data cleaning', {
-  
-  df <- data.frame("operation_number" = c(1, 2, 2))
-  testthat::expect_error(test.ccl_dm(df))
-  
-  df <- data.frame("operation_number" = c(1, 2, 3),
-                   "infant" = c(0, 1, "string"))
-  testthat::expect_error(test.ccl_dm(df))
-  
-  df <- data.frame("operation_number" = c(1, 2, 3),
-                   "infant" = c(0, 1, 0),
-                   "licensed_capacity" = c(10, NA, "string"))
-  testthat::expect_error(test.ccl_dm(df))
-  
-  df <- data.frame("operation_number" = c(1, 2, 3),
-                   "infant" = c(0, 1, 0),
-                   "licensed_capacity" = c(10, NA, 3),
-                   "center_prvdr" = c(0,1,NA))
-  testthat::expect_error(test.ccl_dm(df))
-  
-  df <- data.frame("operation_number" = c(1, 2, 3),
-                   "infant" = c(0, 1, 0),
-                   "licensed_capacity" = c(10, NA, 3),
-                   "center_prvdr" = c(0,1,1),
-                   "after_school" = c(0,1,"string"))
-  testthat::expect_error(test.ccl_dm(df))
-  
-  df <- data.frame("operation_number" = c(1, 2, 3),
-                   "infant" = c(0, 1, 0),
-                   "licensed_capacity" = c(10, NA, 3),
-                   "center_prvdr" = c(0,1,1),
-                   "after_school" = c(0,1,1))
-  testthat::expect_true(test.ccl_dm(df))
-})
-
-testthat::test_that('Test county conversion function', {
+testthat::test_that('Test county column conversion function', {
 
   df <- data.frame(county = c("TARRANT", "HARRIS", "EL PASO", "TOM GREEN"))
 
   # Test that we can pass in either a character or numeric FIPS code
-  testthat::expect_is(dm.county_col(df, county = "48439"), "data.frame")
-  testthat::expect_is(dm.county_col(df, county = 48439), "data.frame")
-  testthat::expect_is(dm.county_col(df, county = NULL), "data.frame")
+  testthat::expect_is(col.county(df, county = "48439"), "data.frame")
+  testthat::expect_is(col.county(df, county = 48439), "data.frame")
+  testthat::expect_is(col.county(df, county = NULL), "data.frame")
 
   # Test that the number of rows returned is equal to 1
-  testthat::expect_equal(nrow(dm.county_col(df, county = "48439")), 1)
-  testthat::expect_equal(nrow(dm.county_col(df, county = 48439)), 1)
-  testthat::expect_gte(nrow(dm.county_col(df, county = NULL)), 4)
+  testthat::expect_equal(nrow(col.county(df, county = "48439")), 1)
+  testthat::expect_equal(nrow(col.county(df, county = 48439)), 1)
+  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
 
   # Test that passing a fake FIPS code will return 0 rows
-  testthat::expect_equal(nrow(dm.county_col(df, county = "48438")), 0)
-  testthat::expect_equal(nrow(dm.county_col(df, county = 48438)), 0)
+  testthat::expect_equal(nrow(col.county(df, county = "48438")), 0)
+  testthat::expect_equal(nrow(col.county(df, county = 48438)), 0)
   
   # Test that passing the county name in a different format will still return the
   # intended result
   df <- data.frame(county = c("tarrant", "harris", "el paso", "tom green"))
-  testthat::expect_equal(nrow(dm.county_col(df, county = "48439")), 1)
-  testthat::expect_equal(nrow(dm.county_col(df, county = 48439)), 1)
-  testthat::expect_gte(nrow(dm.county_col(df, county = NULL)), 4)
+  testthat::expect_equal(nrow(col.county(df, county = "48439")), 1)
+  testthat::expect_equal(nrow(col.county(df, county = 48439)), 1)
+  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
   
   # Test that passing the county name in a different format will still return the
   # intended result
   df <- data.frame(county = c("tarrant", "harris ", "el paso", "tom green"))
-  testthat::expect_equal(nrow(dm.county_col(df, county = "48201")), 1)
-  testthat::expect_equal(nrow(dm.county_col(df, county = 48201)), 1)
-  testthat::expect_gte(nrow(dm.county_col(df, county = NULL)), 4)
+  testthat::expect_equal(nrow(col.county(df, county = "48201")), 1)
+  testthat::expect_equal(nrow(col.county(df, county = 48201)), 1)
+  testthat::expect_gte(nrow(col.county(df, county = NULL)), 4)
 
   # Test that passing county as a name will fail
-  testthat::expect_error(dm.county_col(df, county = "tarrant"))
-  testthat::expect_error(dm.county_col(df, county = "Tarrant"))
-
+  testthat::expect_error(col.county(df, county = "tarrant"))
+  testthat::expect_error(col.county(df, county = "Tarrant"))
+  
 })
