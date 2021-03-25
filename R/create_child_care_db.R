32d7afd50dadc16ee38af809cbf3236c83c1371d
Joli Holmes <jh111@rice.edu>
2021-03-25 12:28:11 -0500

    updated parameter name to be more accurate

--- R/create_child_care_db.R
+++ R/create_child_care_db.R
@@ -1,14 +1,14 @@
 #' @title Create child care data base
 #' @param root string. Path to the root directory to create the DB
-#' @param census_tbls. List of census tables to pull
+#' @param acs_tbls. List of census tables to pull
 #' @examples
 #' \dontrun{
 #' census_tbls <- list(B23008 = list(year = 2019, state = 48, 
 #'                     geography = "tract", county = 439))
 #' root <- "C:/"
-#' childcare_db(census_tbls = census_tbls, root = root)
+#' childcare_db(acs_tbls = acs_tbls, root = root)
 #' }
-childcare_db <- function(census_tbls,
+childcare_db <- function(acs_tbls,
                          root) {
 
   pth_data <- file.path(root, "data")
@@ -19,7 +19,9 @@ childcare_db <- function(census_tbls,
 
   create_folder_str(pths = pths)
 
-  dwnld.acs(tbls = census_tbls,
-            pth = pth_raw)
+  acs_tbls <- list(tbls = acs_tbls,
+                    pth_raw = pth_raw)
+
+  process.acs(acs_tbls = acs_tbls)
 
 }
