e385cb945dd2d954d934c1f748756ff34dc729e3
Joli Holmes <jh111@rice.edu>
2021-03-25 12:53:39 -0500

    refactored code to be able to pull download and data management steps into one function

--- R/census_dm.R
+++ R/census_dm.R
@@ -37,17 +37,17 @@ test_attr <- function(attr) {
 #' @description Passes in a list of parameters to download the ACS census
 #' data using functions from the tidycensus package
 #' @param tbls list. List of census tables with attributes to download
-#' @param pth_raw. Path to save the raw data.
+#' @param raw_pth. Path to save the raw data.
 #' @details To find a list of parameters to pass see documentation for 
 #' tidycensus::get_acs
 #' @examples 
 #' \dontrun{
 #' tbls <- list(B23008 = list(year = 2019, state = 48, 
 #'                                geography = "tract", county = 439))
-#' pth_raw <- "C:/"
-#' dwnld.acs(tbls = tbls)
+#' raw_pth <- "C:/"
+#' dwnld.acs(tbls = tbls, raw_pth = raw_pth)
 #' }
-dwnld.acs <- function(tbls, pth_raw) {
+dwnld.acs <- function(tbls, raw_pth, ...) {
 
   check_census_key()
 
@@ -68,8 +68,8 @@ dwnld.acs <- function(tbls, pth_raw) {
            return(structure(attr, class = name))
          },
          tbls = tbls,
-         pth = pth_raw,
-         USE.NAMES = FALSE,
+         pth = raw_pth,
+         USE.NAMES = TRUE,
          simplify = FALSE)
 }
 
@@ -97,7 +97,7 @@ dm.B23008 <- function(x) {
     tidyr::gather(variable2, value2, -c(tract, variable, estimate)) %>%
     dplyr::filter(value2) %>%
     dplyr::group_by(tract, variable2) %>%
-    dplyr::summarise(estimate = sum(estimate)) %>% 
+    dplyr::summarise(estimate = sum(estimate)) %>%
     tidyr::spread(variable2, estimate) %>%
     dplyr::mutate(n_kids_lt5 = 5/6*n_kids_lt6,
                   n_kids_working_parents_lt5 = 5/6*n_kids_working_parents_lt6)
@@ -134,7 +134,7 @@ dm.B17024 <- function(x) {
                   n_kids_lt5_under200pct = 5/6*n_kids_lt6_under200pct,
                   pct_kids_lt6_under200_pct = (n_kids_lt6_under200pct/n_kids_lt6)*100,
                   pct_kids_lt5_under200_pct = (n_kids_lt5_under200pct/n_kids_lt5)*100)
-  
+
   assertthat::assert_that(max(df$pct_kids_lt5_under200_pct, na.rm = TRUE) <= 100)
   assertthat::assert_that(all(df$n_kids_lt5_under200pct <= df$n_kids_lt5))
   assertthat::assert_that(all(df$n_kids_lt6_under200pct <= df$n_kids_lt6))
@@ -144,31 +144,35 @@ dm.B17024 <- function(x) {
   return(x)
 }
 
-#' @title Children in poverty with Working Parents
-#' @param B17024 B17024 table from census
-#' @param B23008 B23008 table from census
-#' @param pov_rate children w/ working parents are (1-pov_rate) more likely to be under 200% of the poverty line. Default is .85.
-df_demand <- function(B17024, 
-                      B23008, 
-                      pov_rate = .85) {
+#' @title Data management steps to create the demand table
+#' @description Children in poverty with working parents
+#' @param pov_rate children w/ working parents are (1-pov_rate) more likely 
+#' to be under 200% of the poverty line. Default is .85.
+dm.demand <- function(tbls,
+                      processed_pth,
+                      name = "demand",
+                      pov_rate = .85,
+                      ...) {
 
-  df <- B17024 %>%
-    dplyr::inner_join(B23008) %>%
+  df <- tbls$B17024$df %>%
+    dplyr::inner_join(tbls$B23008$df) %>%
     dplyr::mutate(working_pov_rate = pov_rate * pct_kids_lt6_under200_pct) %>%
     dplyr::mutate(n_kids_lt6_working_under200_pct = (working_pov_rate/100) * n_kids_working_parents_lt6) %>%
     dplyr::mutate(n_kids_lt5_working_under200_pct = (working_pov_rate/100) * n_kids_working_parents_lt5)
 
-  assertthat::assert_that(df$working_pov_rate <= 100)
-  assertthat::assert_that(all(df$n_kids_lt5_working_under200_pct <= df$n_kids_lt6_working_under200_pct))
+  assertthat::assert_that(all(df$working_pov_rate <= 100, na.rm = TRUE))
+  assertthat::assert_that(all(df$n_kids_lt5_working_under200_pct <= df$n_kids_lt6_working_under200_pct, na.rm = TRUE))
+
+  readr::write_csv(df, file.path(processed_pth, paste(name, "csv", sep = ".")))
 
   return(df)
 }
 
+#' @title Process acs data
+#' @description Process acs data and create demand dataframe
 process.acs <- function(acs_tbls) {
 
-  acs_tbls <- do.call(dwnld.acs, acs_tbls)
-
-  dfs <- lapply(acs_tbls, dm)
-
+  acs_tbls$tbls <- do.call(dwnld.acs, acs_tbls)
+  acs_tbls$tbls <- lapply(acs_tbls$tbls, dm)
+  do.call(dm.demand, acs_tbls)
 }
-
