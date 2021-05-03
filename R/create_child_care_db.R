d50d2678734e9c939cc92af003e6bb575fccd4b3
Joli Holmes <jh111@rice.edu>
2021-05-03 15:07:31 -0500

    saved as an environment

--- R/create_child_care_db.R
+++ R/create_child_care_db.R
@@ -4,8 +4,7 @@ acs_tables <- function(acs_year,
                        acs_state_code,
                        acs_geography,
                        acs_county,
-                       raw_pth,
-                       processed_pth) {
+                       raw_pth) {
 
   list(tbls = 
          list(B23008 = list(year = acs_year,
@@ -17,9 +16,7 @@ acs_tables <- function(acs_year,
                             geography = acs_geography,
                             county = acs_county)
          ),
-         raw_pth = raw_pth,
-         processed_pth = processed_pth
-  )
+         raw_pth = raw_pth)
 }
 
 #' @title Create child care data base
@@ -39,37 +36,26 @@ childcare_db <- function(root,
                          acs_year = 2019,
                          acs_state_code = 48,
                          acs_geography = "tract",
-                         acs_county = 439,
+                         acs_county = NULL,
                          db_name = "child_care_env.Rdata") {
   env <- new.env()
   data_pth <- file.path(root, "data")
   raw_pth <- file.path(data_pth, "raw")
   processed_pth <- file.path(data_pth, "processed")
-
   pths <- c(data_pth, raw_pth, processed_pth)
 
   create_folder_str(pths = pths)
 
-  # process.acs(acs_year = acs_year,
-  #             acs_state_code = acs_state_code,
-  #             acs_geography = acs_geography,
-  #             acs_county = acs_county,
-  #             raw_pth = raw_pth,
-  #             processed_pth = processed_pth)
-  # 
-  # acf <- list(raw_pth = raw_pth,
-  #             processed_pth = processed_pth)
-  # 
-  # process.acf(acf = acf)
-  # 
-  # hhsc_ccl <- list(raw_pth = raw_pth,
-  #             processed_pth = processed_pth)
-  # 
-  # process.hhsc_ccl(hhsc_ccl = hhsc_ccl)
+  env$DEMAND <- process.acs(acs_year = acs_year,
+                            acs_state_code = acs_state_code,
+                            acs_geography = acs_geography,
+                            acs_county = acs_county,
+                            raw_pth = raw_pth)
 
-  tracts_xwalk <- list(raw_pth = raw_pth)
+  env$HHSC_CCL <- process.hhsc_ccl(cls = list(raw_pth = raw_pth,
+                                              name = "HHSC_CCL"))
 
-  env$tracts_xwalk <- process.tracts_xwalk(tracts_xwalk = tracts_xwalk)
+  env$TRACTS_XWALK <- process.tracts_xwalk(cls = list(raw_pth = raw_pth))
 
   save(env, file = file.path(processed_pth, db_name))
 }
