2a8d841b77497e9956c77428c44a7f4ff88319cc
Joli Holmes <jh111@rice.edu>
2021-05-06 11:53:47 -0500

    modularized the functions further

--- R/calculate_tract_radius.R
+++ R/calculate_tract_radius.R
@@ -33,7 +33,7 @@ distance_between_zips <- function(df,
 #' @param max_child_age. Integer. Age to filter the child age to be less than or
 #' equal.
 #' @return data.frame
-dm.acf_dist <- function(df = DF_ACF,
+dm.acf_dist <- function(df,
                         max_child_age = 4) {
   
   df <- df %>%
@@ -48,48 +48,48 @@ dm.acf_dist <- function(df = DF_ACF,
   return(df)
 }
 
+#' @title Distance density plot
+#' @param df. data.frame. Default is DF_ACF.
+#' @param county_list. A list of TX county codes of counties of interest to 
+#' compare. E.g county_list = c(48439) for Tarrant County; 
+#' county_list = c("48201", "48439") to compare Harris to Tarrant County
+#' @return plot
+distance_density_plot <- function(df = DF_ACF, county_list) {
+
+  summary.acf_dist(df = df, county_list = county_list) %>%
+    dplyr::filter(distance <= 10) %>% 
+    ggplot2::ggplot() +
+    ggplot2::geom_density(ggplot2::aes(x = distance,
+                                       fill = quarter_year), alpha = .5) +
+    ggplot2::theme_minimal() +
+    ggplot2::facet_grid(. ~ family_fips_code)
+}
+
+#' @title Distance decile table
+#' @param df. data.frame. Default is DF_ACF.
+#' @param county_list. A list of TX county codes of counties of interest to 
+#' compare. E.g county_list = c(48439) for Tarrant County; 
+#' county_list = c("48201", "48439") to compare Harris to Tarrant County
+#' @return table
+distance_decile_table <- function(df = DF_ACF, county_list) {
+
+  summary.acf_dist(df = df, county_list = county_list) %>%
+    dplyr::summarise(decile = list(quantile(distance, prob = seq(0, 1, .1), 
+                                            na.rm = TRUE))) %>% 
+    tidyr::unnest_wider(col = decile)
+}
+
 #' @title ACF data management to get zip codes for providers
 #' @description Clean ACF provider data, find coordinates for each zip code, 
 #' calculate straight line distance.
-#' @param df data.frame. The merged family/home ACF data from dm.acf_dist 
-#' function.
+#' @param df data.frame.
 #' @param county_list. A list of TX county codes of counties of interest to 
-#' compare. 
-#' Eg county_list = c(439) for Tarrant County; county_list = c(201, 439) to 
-#' compare Harris to Tarrant County
-#' @return a table and a plot. Decile values for distance between home and 
-#' provider zip code and density plot of distances.
-summary.acf_dist <- function(df = DF_ACF,
-                             county_list = NULL) {
-
-  df <- df %>% 
-    dplyr::left_join(XWALK_ZIP_COUNTY, by = c("family_zip" = "zip")) %>% 
-    dplyr::left_join(XWALK_ZIP_COUNTY, by = c("provider_zip" = "zip"), 
-                     suffix = c("_family", "_provider"))
-
-  subset <- df %>%
-    dplyr::filter(county_family %in% county_list & county_provider %in% county_list & county_family == county_provider) %>% 
-    dplyr::left_join(tigris::fips_codes %>% 
-                       dplyr::filter(state_code == 48) %>%
-                       dplyr::mutate(county_code = as.numeric(county_code)),
-                     by = c("county_family" ="county_code")) %>% 
-    dplyr::rename(county_label = county)
-
-  assertthat::assert_that(all(unique(subset$county_family) %in% county_list))
-  assertthat::assert_that(all(unique(subset$county_provider) %in% county_list))
-  assertthat::assert_that(all(subset$county_provider == subset$county_provider))
+#' compare. E.g county_list = c(48439) for Tarrant County; 
+#' county_list = c("48201", "48439") to compare Harris to Tarrant County
+summary.acf_dist <- function(df, county_list) {
   
-  assertthat::assert_that(sum(is.na(subset$distance)) == 0,
-                          msg = "NAs in the distance")
-  
-  density_plot <- ggplot2::ggplot(subset) +
-    ggplot2::geom_density(ggplot2::aes(x = distance, group=county_label, fill = county_label), alpha = .5) +
-    ggplot2::theme_minimal()
-
-  decile_table <- subset %>% 
-    dplyr::group_by(county_label) %>% 
-    dplyr::summarise(decile = list(quantile(distance, prob = seq(0, 1, .1)))) %>% 
-    tidyr::unnest_wider(col = decile)
-  
-  return(list(density_plot, decile_table))
+  df <- df %>%
+    dplyr::filter(family_fips_code %in% county_list) %>%
+    dm.acf_dist() %>% 
+    dplyr::group_by(family_fips_code, quarter_year)
 }
