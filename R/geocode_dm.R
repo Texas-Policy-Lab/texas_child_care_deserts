e3fbc0dc08467cc3da0fadc33a9f3393b10b6905
Joli Holmes <jh111@rice.edu>
2021-05-10 16:26:48 -0500

    updated functions

--- R/geocode_dm.R
+++ R/geocode_dm.R
@@ -1,14 +1,30 @@
 #' @title Pulls down bounding box parameters for Texas
 #' @export
-tx_bounding_box <- function(url = "https://gist.githubusercontent.com/a8dx/2340f9527af64f8ef8439366de981168/raw/81d876daea10eab5c2675811c39bcd18a79a9212/US_State_Bounding_Boxes.csv",
+tx_bounding_box <- function(df,
+                            url = "https://gist.githubusercontent.com/a8dx/2340f9527af64f8ef8439366de981168/raw/81d876daea10eab5c2675811c39bcd18a79a9212/US_State_Bounding_Boxes.csv",
                             state_fips) {
 
-  df <- readr::read_csv(url) %>%
+  bb <- readr::read_csv(url) %>%
     dplyr::filter(STATEFP == state_fips)
 
-  return(list(ul = list(lng = df$xmin, lat = df$ymax),
-              lr = list(lng = df$xmax, lat = df$ymin))
+  return(list(ul = list(lng = bb$xmin, lat = bb$ymax),
+              lr = list(lng = bb$xmax, lat = bb$ymin))
   )
+}
+
+#' @title Check Texas Bounds
+#' @description Checks that non-missing lat and longitudes are within the Texas
+#' state boundaries and if they are not it assign an NA
+#' @return data.frame
+check_tx_bounds <- function(df,
+                            bb) {
+
+  df %>%
+    dplyr::mutate(lat = as.numeric(lat),
+                  long = as.numeric(long),
+                  lat = ifelse(lat >= bb$lr$lat & lat <= bb$ul$lat, lat, NA),
+                  long = ifelse(long >= bb$ul$lng & long <= bb$lr$lng, long, NA)
+                  )
 }
 
 #' @title Split calls
@@ -30,29 +46,29 @@ split_calls <- function(v, limit) {
 #' @title Response -> Dataframe
 #' @description Turns the response from mapquest into a dataframe
 #' @export
-dm.geocode_request <- function(c) {
+dm.geocode_request <- function(results, call) {
 
-  l <- lapply(1:length(c$results), function(x) {
+  l <- lapply(1:length(results), function(x) {
 
-    row <- c$results[[x]]$locations[[1]]
+    row <- results[[x]]$locations[[1]]
 
-    df <- data.frame(street = row$street,
-                     neighborhood = row$adminArea6,
-                     city  = row$adminArea5,
-                     county = row$adminArea4,
-                     state = row$adminArea3,
-                     zip = row$postalCode,
-                     lat = row$latLng$lat,
-                     long = row$latLng$lng,
-                     geocodeQualityCode = row$geocodeQualityCode,
-                     mapURl= row$mapUrl,
-                     stringsAsFactors = FALSE
+    data.frame(
+               street = row$street,
+               neighborhood = row$adminArea6,
+               city  = row$adminArea5,
+               county = row$adminArea4,
+               state = row$adminArea3,
+               zip = row$postalCode,
+               lat = row$latLng$lat,
+               long = row$latLng$lng,
+               geocodeQualityCode = row$geocodeQualityCode,
+               mapURl= row$mapUrl,
+               stringsAsFactors = FALSE
     )
-  })
+    }) %>% 
+    dplyr::bind_rows() %>%
+    dplyr::bind_cols(address = call)
 
-  df <- do.call(rbind, l)
-
-  return(df)
 }
 
 #' @title Drops poor quality geocodes
@@ -61,14 +77,12 @@ dm.geocode_request <- function(c) {
 dm.drop_poor_quality <- function(df,
                                  qualityCode = "A1|A3|A4") {
 
-  poorQuality <- stringr::str_starts(string = df$geocodeQualityCode, pattern = qualityCode)
+  poorQuality <- stringr::str_starts(string = df$geocodeQualityCode, 
+                                     pattern = qualityCode)
 
-  df <- df %>%
+  df %>%
     dplyr::mutate(lat = ifelse(poorQuality, NA, lat),
-                  long = ifelse(poorQuality, NA, long)
-    )
-
-  return(df)
+                  long = ifelse(poorQuality, NA, long))
 }
 
 #' @title Geocode addresses
@@ -79,47 +93,57 @@ dm.drop_poor_quality <- function(df,
 dm.geocode_address <- function(df,
                                state_fips,
                                version = "v1",
-                               url = "http://www.mapquestapi.com/geocoding/{version}/batch?key={key}",
+                               url = "http://www.mapquestapi.com",
+                               path = "/geocoding/v1/batch",
                                limit = 100) {
-  
-  get_key.mapquest()
 
-  bb <- tx_bounding_box(state_fips = state_fips)
+  bb <- tx_bounding_box(df = df,
+                        state_fips = state_fips)
 
-  calls <- split_calls(v = df$address,
+  subset <- dm.subset_ccl_geocode(df, bb)
+
+  calls <- split_calls(v = subset$address,
                        limit = limit)
-  
-  l <- lapply(calls, function(call, url, version) {
-    r <- httr::POST(url = glue::glue(url, version = version, key = ),
+
+  key <- get_key.mapquest()
+  url <- httr::modify_url(url = url, path = path)
+
+  l <- lapply(calls, function(call, url, key) {
+
+    r <- httr::POST(url = url,
                     query = list(key = key),
                     body = list(locations = call,
                                 boundingBox = bb,
-                                maxResults = 1),
+                                maxResults = 1,
+                                outFormat ="json"),
                     encode = "json")
+  
+    if (r$status_code == 200) {
 
-    if(r$status_code == 200) {
+      if (httr::http_type(r) != "application/json") {
+        stop("API did not return json", call. = FALSE)
+      }
 
-      c <- httr::content(r)
-
-      df <- dm.geocode_request(c)
-
-      return(df)
-
+      dm.geocode_request(results = httr::content(r)$results,
+                         call)
     } else {
       warning("status not 200")
     }
 
-  }, url = url, version = version, key = key)
+  }, url = url, key = key) %>%
+    dplyr::bind_rows() %>%
+    dplyr::bind_cols(subset %>%
+                        dplyr::select(operation_number)) %>%
+    dm.drop_poor_quality() %>%
+    dplyr::mutate(lat2 = as.character(lat),
+                  long2 = as.character(long)) %>% 
+    dplyr::select(operation_number, lat2, long2)
 
-  df <- do.call(rbind, l)
-
-  df <- df %>%
-    dplyr::bind_cols(addresses %>%
-                       dplyr::select(operation_number))
-
-  df <- dm.drop_poor_quality(df)
-
-  return(df)
+  df %>%
+    dplyr::left_join(l) %>% 
+    dplyr::mutate(lat = ifelse(is.na(lat), lat2, lat),
+                  long = ifelse(is.na(long), long2, long)) %>% 
+    dplyr::select(-c(lat2, long2))
 }
 
 #' @title Geocode addresses
@@ -135,10 +159,11 @@ dm.reverse_geocode <- function(latLng,
                                version = "v1",
                                url = "http://www.mapquestapi.com/geocoding/{version}/reverse?key={key}") {
 
-  get_key.mapquest()
-  
-  l <- lapply(latLng, function(x, url, version) {
-    r <- httr::POST(url = glue::glue(url, version = version, key = key),
+  key <- get_key.mapquest()
+
+  l <- lapply(latLng, function(x, url, version, key) {
+    r <- httr::POST(url = glue::glue(url, version = version, 
+                                     key = key),
                     query = list(key = key),
                     body = list(location = list(latLng = x)),
                     encode = "json")
@@ -155,16 +180,16 @@ dm.reverse_geocode <- function(latLng,
 
   }, url = url, version = version, key = key)
 
-  df <- do.call(rbind, l)
-
-  return(df)
+  l %>% 
+    dplyr::bind_rows()
 }
 
 #' @title Subset CCL for geocoding
-dm.subset_ccl_geocode <- function(df) {
+dm.subset_ccl_geocode <- function(df, bb) {
 
   df %>%
+    check_tx_bounds(bb) %>%
     dplyr::filter(is.na(lat) | is.na(long)) %>%
-    dplyr::select(opeartion_number, address)
+    dplyr::select(operation_number, address)
 
 }
