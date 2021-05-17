494ee092a5a89a41a1e2d13b2b997a65ad678550
Joli Holmes <jh111@rice.edu>
2021-05-17 13:57:42 -0500

    added demand functions

--- R/child_care_markets.R
+++ R/child_care_markets.R
@@ -11,14 +11,27 @@ drop_bottom_1pct <- function(df) {
   return(df)
 }
 
+#' @title Create tract demand
+create_tract_demand <- function(demand) {
+  
+  demand %>%
+    dplyr::select(tract, county_code, n_kids_working_parents_lt5, n_kids_lt5_working_under200_pct) %>%
+    dplyr::rename(all_provider = n_kids_working_parents_lt5,
+                  sub_provider = n_kids_lt5_working_under200_pct) %>% 
+    dplyr::mutate(sub_trs4_provider = sub_provider,
+                  sub_trs_provider = sub_provider) %>% 
+    tidyr::pivot_longer(names_to = "desert", values_to = "tract_demand", cols = -c(tract, county_code))
+
+}
+
 #' @title Create Market demand
 create_market_demand <- function(demand, tracts) {
 
   tracts %>%
     dplyr::inner_join(demand, by = c("surround_tract" = "tract")) %>% 
-    dplyr::group_by(anchor_county, anchor_tract) %>% 
-    dplyr::summarise(all_provider = sum(n_kids_working_parents_lt5, na.rm = T),
-                     sub_provider = sum(n_kids_lt5_working_under200_pct, na.rm = T))
+    dplyr::group_by(anchor_county, anchor_tract, desert) %>% 
+    dplyr::summarise(mkt_demand = sum(tract_demand, na.rm = T)) %>%
+    dplyr::ungroup()
 }
 
 #' @title Create market supply
