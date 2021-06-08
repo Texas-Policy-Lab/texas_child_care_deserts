9bf5068e56c724e59a995bcdc95bddc9bf9a7686
Joli Holmes <jh111@rice.edu>
2021-06-08 12:13:07 -0500

    fixed bug in tract ids

--- R/tracts_dm.R
+++ R/tracts_dm.R
@@ -6,17 +6,17 @@
 #' @export
 dwnld.xwalk_tracts <- function(raw_pth,
                                url = "http://data.nber.org/distance/2010/sf1/tract/sf12010tractdistance25miles.csv") {
-
+  
   fl <- basename(url)
-
+  
   if (!(fl %in% list.files(raw_pth))) {
-
+    
     httr::GET(url, httr::write_disk(temp_dir <- tempfile(fileext = ".csv")))
     file.copy(from = temp_dir, to = file.path(raw_pth, fl))
   }
-
+  
   df <- read.csv(file.path(raw_pth, fl))
-
+  
   return(df)
 }
 
@@ -26,43 +26,45 @@ dwnld.xwalk_tracts <- function(raw_pth,
 #' @param pth string. The name of the data to read in.
 #' @param name string. The name of the data to write out.
 #' @export
-dm.xwalk_tracts <- function(x, tract_side = "right", county_side = "left", 
-                            tract_width = 11, county_width = 5, pad = "0") {
-
+dm.xwalk_tracts <- function(x, 
+                            tract_side = "left", 
+                            county_side = "left", 
+                            tract_width = 6, county_width = 5, pad = "0") {
+  
   df <- x$df %>%
     dplyr::mutate(texas = ifelse(substr(county1, 1, 2) == "48", TRUE, FALSE)) %>%
     dplyr::filter(texas) %>%
     dplyr::select(-texas) %>%
-    dplyr::mutate(anchor_tract = stringr::str_pad(paste0(county1, tract1), 
-                                                  side = tract_side, 
-                                                  width = tract_width,
-                                                  pad = pad),
-                  surround_tract = stringr::str_pad(paste0(county2, tract2), 
-                                                    side = tract_side, 
-                                                    width = tract_width,
-                                                    pad = pad),
-                  anchor_county = stringr::str_pad(county1, 
+    dplyr::mutate(anchor_county = stringr::str_pad(county1, 
                                                    side = county_side, 
                                                    width = county_width, 
                                                    pad = pad),
                   surround_county = stringr::str_pad(county2, 
                                                      side = county_side,
                                                      width = county_width, 
-                                                     pad = pad))
-
+                                                     pad = pad),
+                  anchor_tract = paste0(anchor_county, stringr::str_pad(tract1, 
+                                                                        side = tract_side, 
+                                                                        width = tract_width,
+                                                                        pad = pad)),
+                  surround_tract = paste0(surround_county, stringr::str_pad(tract2, 
+                                                                            side = tract_side, 
+                                                                            width = tract_width,
+                                                                            pad = pad)))
+  
   tracts <- df %>%
     dplyr::distinct(anchor_tract, anchor_county) %>%
     dplyr::mutate(surround_tract = anchor_tract,
                   surround_county = anchor_county,
                   mi_to_tract = 0)
-
+  
   df <- df %>%
     dplyr::bind_rows(tracts) %>% 
     dplyr::select(anchor_county, anchor_tract, 
                   surround_county, surround_tract, mi_to_tract)
-
-  assertthat::assert_that(all(nchar(df$anchor_tract) == tract_width))
-  assertthat::assert_that(all(nchar(df$surround_tract) == tract_width))
+  
+  assertthat::assert_that(all(nchar(df$anchor_tract) == 11))
+  assertthat::assert_that(all(nchar(df$surround_tract) == 11))
   assertthat::assert_that(all(nchar(df$anchor_county) == 5))
   assertthat::assert_that(all(nchar(df$surround_county) == 5))
   
@@ -71,7 +73,7 @@ dm.xwalk_tracts <- function(x, tract_side = "right", county_side = "left",
 
 #' @title Process ACF data
 process.tracts_xwalk <- function(cls) {
-
+  
   cls$df <- dwnld.xwalk_tracts(raw_pth = cls$raw_pth)
   tracts_xwalk <- dm.xwalk_tracts(cls)
 }
