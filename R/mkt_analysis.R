5add064b73ff6adeade4e5f153c5506adf0cfe76
Joli Holmes <jh111@rice.edu>
2021-06-17 09:15:27 -0500

    pivoted the tables wider so it is easier to summarize in the app

--- R/mkt_analysis.R
+++ R/mkt_analysis.R
@@ -3,47 +3,52 @@
 pct_desert <- function(df) {
 
   df %>%
-    dplyr::group_by(desert_type, desert) %>% 
+    dplyr::group_by(desert_type, desert) %>%
     dplyr::summarise(n = dplyr::n()) %>% 
-    dplyr::mutate(pct = round((n/sum(n))*100,1)) %>% 
-    dplyr::filter(desert) %>% 
-    dplyr::pull(pct)
+    dplyr::mutate(pct = round((n/sum(n))*100,1)) %>%
+    dplyr::filter(desert) %>%
+    dplyr::select(-n) %>%
+    tidyr::pivot_wider(names_from = desert_type, values_from = pct)
 }
 
 #' @title Find total number of children in need of care of each type
 #' @export
 total_children <- function(df) {
   
-  df %>% 
-    dplyr::group_by(desert) %>% 
-    dplyr::summarise(n_kids = round(sum(tract_demand, na.rm = T)))
+  df %>%
+    dplyr::group_by(desert) %>%
+    dplyr::summarise(n_kids = round(sum(tract_demand, na.rm = T))) %>%
+    tidyr::pivot_wider(names_from = desert, values_from = n_kids)
 }
 
 #' @title Find total number of seats of each type
 #' @export
 total_seats <- function(df){
-  
-  df %>% 
-    dplyr::group_by(desert) %>% 
-    dplyr::summarise(n_seats = round(sum(tract_supply, na.rm = T)))
+
+  df %>%
+    dplyr::group_by(desert) %>%
+    dplyr::summarise(n_seats = round(sum(tract_supply, na.rm = T))) %>%
+    tidyr::pivot_wider(names_from = desert, values_from = n_seats)
 }
 
 #' @title Find average number of children per market
 #' @export
 avg_children_mkt <- function(df) {
-  
-  df %>% 
-    dplyr::group_by(desert_type) %>% 
-    dplyr::summarise(n_kids = round(mean(mkt_demand, na.rm = T)))
+
+  df %>%
+    dplyr::group_by(desert_type) %>%
+    dplyr::summarise(n_kids = round(mean(mkt_demand, na.rm = T))) %>%
+    tidyr::pivot_wider(names_from = desert_type, values_from = n_kids)
 }
 
 #' @title Find average number of seats per market
 #' @export
 avg_seats_mkt <- function(df){
-  
-  df %>% 
-    dplyr::group_by(desert_type) %>% 
-    dplyr::summarise(n_seats = round(mean(mkt_supply, na.rm = T)))
+
+  df %>%
+    dplyr::group_by(desert_type) %>%
+    dplyr::summarise(n_seats = round(mean(mkt_supply, na.rm = T))) %>%
+    tidyr::pivot_wider(names_from = desert_type, values_from = n_seats)
 }
 
 #' @title Find average number of providers per market
@@ -62,13 +67,14 @@ total_children_desert <- function(df_ratio,
                                   df_demand) {
   
   df_ratio %>% 
-    dplyr::filter(desert) %>% 
-    dplyr::select(anchor_tract, desert_type) %>% 
-    dplyr::left_join(XWALK_TRACTS) %>% 
-    dplyr::distinct(surround_tract, desert_type) %>% 
+    dplyr::filter(desert) %>%
+    dplyr::select(anchor_tract, desert_type) %>%
+    dplyr::left_join(XWALK_TRACTS) %>%
+    dplyr::distinct(surround_tract, desert_type) %>%
     dplyr::inner_join(df_demand, by = c("surround_tract" = "tract",
-                                        "desert_type" = "desert"))%>% 
-    dplyr::group_by(desert_type) %>% 
-    dplyr::summarise(n_kids = round(sum(tract_demand, na.rm = T)))
+                                        "desert_type" = "desert")) %>%
+    dplyr::group_by(desert_type) %>%
+    dplyr::summarise(n_kids = round(sum(tract_demand, na.rm = T))) %>%
+    tidyr::pivot_wider(names_from = desert_type, values_from = n_kids)
 }
 
