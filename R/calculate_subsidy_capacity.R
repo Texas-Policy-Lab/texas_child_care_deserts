ce03f47e9091d5fd72bed8b88afb42719100c406
Sadie H Richardson <shr2@rice.edu>
2021-09-10 08:51:46 -0500

    fix formula mistake

--- R/calculate_subsidy_capacity.R
+++ R/calculate_subsidy_capacity.R
@@ -20,7 +20,7 @@ calc.capacity_adjustment_sub <- function(df_hhsc_ccl,
     dplyr::filter(sub_provider) %>% 
     dplyr::mutate(med_param = (n_subsidy_kids/enrollment_total)*desired_pct_capacity,
                   med_param = ifelse(!is.finite(med_param), NA, med_param),
-                  optimistic_param =  ((n_subsidy_kids + seats_total)/licensed_capacity)*desired_pct_capacity,
+                  optimistic_param =  ((n_subsidy_kids + availability_total)/licensed_capacity)*desired_pct_capacity,
                   pessimistic_param = (n_subsidy_kids/licensed_capacity)*desired_pct_capacity)
   
   df_expectation <- df %>% 
