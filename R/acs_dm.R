ed8462998b5e8dd06469064801f598d41f5ebd71
Joli Holmes <jh111@rice.edu>
2021-09-23 12:22:17 -0500

    changed order of the function so they are called in the order they appear

--- R/acs_dm.R
+++ R/acs_dm.R
@@ -1,3 +1,19 @@
+#' @title Test attributes
+#' @description Test's that the attributes of the parameters passed in are 
+#' correct
+#' @title attr. List of attributes to test
+test_attr <- function(attr) {
+  assertthat::assert_that(c("geography") %in% names(attr),
+                          msg = "Table configuration is missing geography parameter")
+  assertthat::assert_that(any(c("variables", "table") %in% names(attr)),
+                          msg = "Table configuration is missing variables or table parameter")
+  if(all(c("variables", "table") %in% names(attr))) {
+    assertthat::assert_that(all(attr$table == sub("_.*", "", attr$variables)),
+                            msg = "Table name differs from variable root")  
+  }
+  return(TRUE)
+}
+
 #' @title Download ACS
 #' @description Passes in a list of parameters to download the ACS census
 #' data using functions from the tidycensus package
@@ -13,48 +29,32 @@
 #' dwnld.acs(tbls = tbls, raw_pth = raw_pth)
 #' }
 dwnld.acs <- function(tbls, raw_pth, ...) {
-  
+
   get_key.census()
-  
+
   f <- function(name, tbls, pth) {
-    
+
     attr <- tbls[[name]]
     attr$table <- name
-    
+
     test_attr(attr)
-    
+
     df <- do.call(tidycensus::get_acs, attr)
-    
+
     if (!is.null(pth)) {
       readr::write_csv(df, file.path(pth, paste0(name, ".csv")))
     }
-    
+
     attr$df <- df
     return(structure(attr, class = name))
   }
-  
+
   sapply(names(tbls),
          f,
          tbls = tbls,
          pth = raw_pth,
          USE.NAMES = TRUE,
          simplify = FALSE)
-}
-
-#' @title Test attributes
-#' @description Test's that the attributes of the parameters passed in are 
-#' correct
-#' @title attr. List of attributes to test
-test_attr <- function(attr) {
-  assertthat::assert_that(c("geography") %in% names(attr),
-                          msg = "Table configuration is missing geography parameter")
-  assertthat::assert_that(any(c("variables", "table") %in% names(attr)),
-                          msg = "Table configuration is missing variables or table parameter")
-  if(all(c("variables", "table") %in% names(attr))) {
-    assertthat::assert_that(all(attr$table == sub("_.*", "", attr$variables)),
-                            msg = "Table name differs from variable root")  
-  }
-  return(TRUE)
 }
 
 #' @title Choose geography
