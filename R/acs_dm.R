3ab7814e17d2f4629dec29819abdae47008a23fd
Joli Holmes <jh111@rice.edu>
2021-09-23 14:58:17 -0500

    refactored code to make it easier to follow and remove duplicate coding steps

--- R/acs_dm.R
+++ R/acs_dm.R
@@ -18,174 +18,129 @@ test_attr <- function(attr) {
 #' @description Passes in a list of parameters to download the ACS census
 #' data using functions from the tidycensus package
 #' @param tbls list. List of census tables with attributes to download
-#' @param raw_pth. Path to save the raw data.
+#' @param pth. Path to save the raw data.
 #' @details To find a list of parameters to pass see documentation for 
 #' tidycensus::get_acs
 #' @examples 
 #' \dontrun{
-#' tbls <- list(B23008 = list(year = 2019, state = 48, 
+#' x <- list(B23008 = list(year = 2019, state = 48, 
 #'                                geography = "tract", county = 439))
-#' raw_pth <- "C:/"
-#' dwnld.acs(tbls = tbls, raw_pth = raw_pth)
+#' dwnld.acs(x = x)
 #' }
-dwnld.acs <- function(tbls, raw_pth, ...) {
+dwnld.acs <- function(x, ...) {
 
-  get_key.census()
+  test_attr(x)
 
-  f <- function(name, tbls, pth) {
+  x$df <- do.call(tidycensus::get_acs, x)
 
-    attr <- tbls[[name]]
-    attr$table <- name
-
-    test_attr(attr)
-
-    df <- do.call(tidycensus::get_acs, attr)
-
-    if (!is.null(pth)) {
-      readr::write_csv(df, file.path(pth, paste0(name, ".csv")))
-    }
-
-    attr$df <- df
-    return(structure(attr, class = name))
+  if (!is.null(x$pth)) {
+    readr::write_csv(x$df, 
+                     file.path(x$pth,
+                               paste0(x$table, ".csv")))
   }
 
-  sapply(names(tbls),
-         f,
-         tbls = tbls,
-         pth = raw_pth,
-         USE.NAMES = TRUE,
-         simplify = FALSE)
+  return(x)
 }
 
 #' @title Choose geography
 #' @description Applied different data management steps depending on which 
 #' geography is selected
-geo_dm <- function() UseMethod("geo_dm")
+dm_acs_geo <- function(x) UseMethod("dm_acs_geo")
 
 #' @title Data management steps for census tracts geos
-geo_dm.tracts <- function() {
+dm_acs_geo.tract <- function(x) {
 
-  assertthat::assert_that(all(nchar(df$GEOID) == 11))
+  assertthat::assert_that(all(nchar(x$df$GEOID) == 11))
 
-  df <- df %>%
+  x$df <- x$df %>%
     dplyr::rename(tract = GEOID) %>%
     dplyr::mutate(county_code = substr(tract, 1, 5))
 
-  assertthat::assert_that(all(nchar(df$county_code) == 5))
-  assertthat::assert_that(length(unique(df$tract)) == nrow(df))
+  assertthat::assert_that(all(nchar(x$df$county_code) == 5))
+  assertthat::assert_that(length(unique(x$df$tract)) == nrow(x$df))
 
-  return(df)
+  return(x)
 }
 
 #' @title Data management steps for ZCTA geos
-geo_dm.zcta <- function() {
+dm_acs_geo.zcta <- function(x) {
 
-  assertthat::assert_that(all(nchar(df$GEOID)) == 5)
+  assertthat::assert_that(all(nchar(x$df$GEOID)) == 5)
 
-  df <- df %>% 
+  x$df <- x$df %>% 
     dplyr::rename(zip = GEOID)
 
-  assertthat::assert_that(length(unique(df$zip)) == nrow(df))
+  assertthat::assert_that(length(unique(x$df$zip)) == nrow(x$df))
 
-  return(df)  
+  return(x)  
 }
 
 #' @title Data management
 #' @description Data management steps for a specific data table
 #' @param x object. The data object
-dm <- function(x) UseMethod("dm")
+dm_acs <- function(x) UseMethod("dm_acs")
+#' @title Default data managment steps
+dm_acs.default <- function(x) {
+
+  assertthat::assert_that(all(x$other_lt6 %in% x$df$variable), 
+                          msg = "Missing expected variables to create those with working parents")
+
+  x$df <- x$df %>%
+    dplyr::select(GEOID, variable, estimate) %>%
+    dplyr::mutate(n_kids_lt6 = variable %in% x$lt6,
+                  n_kids_other_lt6 = variable %in% x$other_lt6) %>%
+    tidyr::gather(n_kids, value2, -c(GEOID, variable, estimate)) %>%
+    dplyr::filter(value2) %>%
+    dplyr::group_by(GEOID, n_kids) %>%
+    dplyr::summarise(estimate = sum(estimate)) %>%
+    tidyr::spread(n_kids, estimate) %>%
+    dplyr::mutate(n_kids_lt5 = 5/6*n_kids_lt6,
+                  n_kids_lt4 = 4/6*n_kids_lt6)
+
+  assertthat::assert_that(all(x$df$n_kids_lt5 <= x$df$n_kids_lt6))
+  assertthat::assert_that(all(x$df$n_kids_lt4 <= x$df$n_kids_lt6))
+
+  return(x)
+}
 
 #' @title Demand using table B23008
 #' @description Creates variables: Estimated number of children under 5,
 #' estimated number of children less than 6 with working parents, and
 #' estimated number of children less than 5 with working parents
-#' @inheritParams dm
-dm.B23008 <- function(x) {
+#' @inheritParams dm_acs
+dm_acs.B23008 <- function(x) {
 
-  browser()
-  
-  lt6 <- paste(x$table, x$lt6, sep = "_")
-  lt6_working_parents <- paste(x$table, x$vars, sep = "_")
-  
-  assertthat::assert_that(all(lt6_working_parents %in% x$df$variable), msg = "Missing expected variables to create those with working parents")
-  
-  df <- x$df %>%
-    dplyr::select(GEOID, variable, estimate) %>%
-    dplyr::mutate(n_kids_lt6 = ifelse(variable %in% lt6, TRUE, FALSE),
-                  n_kids_working_parents_lt6 = ifelse(variable %in% lt6_working_parents, TRUE, FALSE)) %>%
-    tidyr::gather(variable2, value2, -c(GEOID, variable, estimate)) %>%
-    dplyr::filter(value2) %>%
-    dplyr::group_by(GEOID, variable2) %>%
-    dplyr::summarise(estimate = sum(estimate)) %>%
-    tidyr::spread(variable2, estimate) %>%
-    dplyr::mutate(n_kids_lt5 = 5/6*n_kids_lt6,
-                  n_kids_lt4 = 4/6*n_kids_lt6,
-                  n_kids_working_parents_lt5 = 5/6*n_kids_working_parents_lt6,
+  x$df <- x$df %>%
+    dplyr::rename(n_kids_working_parents_lt6 = n_kids_other_lt6) %>%
+    dplyr::mutate(n_kids_working_parents_lt5 = 5/6*n_kids_working_parents_lt6,
                   n_kids_working_parents_lt4 = 4/6*n_kids_working_parents_lt6)
+  
+  assertthat::assert_that(all(x$df$n_kids_working_parents_lt5 <= x$df$n_kids_working_parents_lt6))
+  assertthat::assert_that(all(x$df$n_kids_working_parents_lt4 <= x$df$n_kids_working_parents_lt6))
+  assertthat::assert_that(all(x$df$n_kids_working_parents_lt6 <= x$df$n_kids_lt6))
 
-  df <- geo_dm(df)
-
-  assertthat::assert_that(all(df$n_kids_lt5 <= df$n_kids_lt6))
-  assertthat::assert_that(all(df$n_kids_lt4 <= df$n_kids_lt6))
-  assertthat::assert_that(all(df$n_kids_working_parents_lt5 <= df$n_kids_working_parents_lt6))
-  assertthat::assert_that(all(df$n_kids_working_parents_lt4 <= df$n_kids_working_parents_lt6))
-  assertthat::assert_that(all(df$n_kids_working_parents_lt6 <= df$n_kids_lt6))
-
-  return(df)
+  return(x)
 }
 
 #' @title Demand using table B17024
 #' @inheritParams dm
-dm.B17024 <- function(x) {
+dm_acs.B17024 <- function(x) {
 
-  lt6 <- paste(x$table, "002", sep = "_")
-  lt6_under200_pct <- paste(x$table, c("003", "004", "005", "006",
-                                       "007", "008", "009", "010"), sep = "_")
-  
-  assertthat::assert_that(all(lt6_under200_pct %in% x$df$variable), msg = "Missing expected variables to create less than 200 pct")
-
-  df <- x$df %>%
-    dplyr::select(GEOID, variable, estimate) %>%
-    dplyr::mutate(n_kids_lt6 = ifelse(variable %in% lt6, TRUE, FALSE),
-                  n_kids_lt6_under200pct = ifelse(variable %in% lt6_under200_pct, TRUE, FALSE)
-    ) %>%
-    tidyr::gather(variable2, value2, -c(GEOID, variable, estimate)) %>%
-    dplyr::filter(value2) %>%
-    dplyr::group_by(GEOID, variable2) %>%
-    dplyr::summarise(estimate = sum(estimate)) %>% 
-    tidyr::spread(variable2, estimate) %>%
-    dplyr::mutate(n_kids_lt5 = 5/6*n_kids_lt6,
+  x$df <- x$df %>%
+    dplyr::rename(n_kids_lt6_under200pct = n_kids_other_lt6) %>%
+    dplyr::mutate(n_kids_lt4_under200pct = 4/6*n_kids_lt6_under200pct,
                   n_kids_lt5_under200pct = 5/6*n_kids_lt6_under200pct,
-                  pct_kids_lt6_under200_pct = (n_kids_lt6_under200pct/n_kids_lt6)*100,
+                  pct_kids_lt4_under200_pct = (n_kids_lt4_under200pct/n_kids_lt4)*100,
                   pct_kids_lt5_under200_pct = (n_kids_lt5_under200pct/n_kids_lt5)*100,
-                  n_kids_lt4 = 4/6*n_kids_lt6,
-                  n_kids_lt4_under200pct = 4/6*n_kids_lt6_under200pct,
-                  pct_kids_lt4_under200_pct = (n_kids_lt4_under200pct/n_kids_lt4)*100)
-                  
-  if (x$geography == "tract") {
-    
-    df <- df %>% 
-      dplyr::rename(tract = GEOID) %>% 
-      dplyr::mutate(county_code = substr(tract, 1, 5))
-    
-    assertthat::assert_that(all(nchar(df$county_code) == 5))
-    
-  } else if (x$geography == "zcta"){
-    
-    df <- df %>% 
-      dplyr::rename(zip = GEOID)
-    
-  } 
+                  pct_kids_lt6_under200_pct = (n_kids_lt6_under200pct/n_kids_lt6)*100)
 
-  assertthat::assert_that(max(df$pct_kids_lt5_under200_pct, na.rm = TRUE) <= 100)
-  assertthat::assert_that(max(df$pct_kids_lt4_under200_pct, na.rm = TRUE) <= 100)
-  assertthat::assert_that(all(df$n_kids_lt5_under200pct <= df$n_kids_lt5))
-  assertthat::assert_that(all(df$n_kids_lt6_under200pct <= df$n_kids_lt6))
-  assertthat::assert_that(all(df$n_kids_lt4_under200pct <= df$n_kids_lt4))
-  assertthat::assert_that(all(df$n_kids_lt5 <= df$n_kids_lt6))
-  assertthat::assert_that(all(df$n_kids_lt4 <= df$n_kids_lt6))
+  assertthat::assert_that(max(x$df$pct_kids_lt5_under200_pct, na.rm = TRUE) <= 100)
+  assertthat::assert_that(max(x$df$pct_kids_lt4_under200_pct, na.rm = TRUE) <= 100)
+  assertthat::assert_that(all(x$df$n_kids_lt5_under200pct <= x$df$n_kids_lt5))
+  assertthat::assert_that(all(x$df$n_kids_lt6_under200pct <= x$df$n_kids_lt6))
+  assertthat::assert_that(all(x$df$n_kids_lt4_under200pct <= x$df$n_kids_lt4))
 
-  return(df)
+  return(x)
 }
 
 #' @title Data management steps to create the demand table
@@ -203,11 +158,11 @@ dm.demand <- function(B17024,
                       pov_rate = .85,
                       ...) {
 
-   assertthat::assert_that(pov_rate <= 1, 
-                           msg = "Poverty rate should be less than or equal to 1")
+  assertthat::assert_that(pov_rate <= 1, 
+                          msg = "Poverty rate should be less than or equal to 1")
 
-  df <- B17024 %>%
-    dplyr::left_join(B23008 %>% 
+  df <- B17024$df %>%
+    dplyr::left_join(B23008$df %>% 
                        dplyr::select(-c(n_kids_lt6, n_kids_lt5, n_kids_lt4))) %>%
     dplyr::mutate(working_pov_rate = pov_rate * pct_kids_lt6_under200_pct) %>%
     dplyr::mutate(n_kids_lt6_working_under200_pct = (working_pov_rate/100) * n_kids_working_parents_lt6) %>%
@@ -217,7 +172,7 @@ dm.demand <- function(B17024,
   assertthat::assert_that(all(df$working_pov_rate <= 100, na.rm = TRUE))
   assertthat::assert_that(all(df$n_kids_lt5_working_under200_pct <= df$n_kids_lt6_working_under200_pct, na.rm = TRUE))
   assertthat::assert_that(all(df$n_kids_lt4_working_under200_pct <= df$n_kids_lt6_working_under200_pct, na.rm = TRUE))
-  
+
   return(df)
 }
 
@@ -228,28 +183,36 @@ acs_tables <- function(acs_year,
                        acs_geography,
                        acs_county,
                        lt6 = "002",
-                       raw_pth) {
+                       pth) {
 
-  f <- function() {
-    list(year = acs_year,
-         state = acs_state_code,
-         geography = acs_geography,
-         county = acs_county,
-         lt6 = lt6)
+  cls <- function(table, geography, lt6, other_lt6, pth) {
+    structure(
+      list(table = table,
+           year = acs_year,
+           state = acs_state_code,
+           geography = acs_geography,
+           county = acs_county,
+           lt6 = paste(table, lt6, sep = "_"),
+           other_lt6 = paste(table, other_lt6, sep = "_"),
+           pth = pth),
+      class = c(table, geography))
   }
-  
-  B23008 <- structure(c(f(),
-                        lt6_working_parents = c("004", "005", "006", 
-                                                "010", "013")),
-                      class = c("B23008", acs_geography))
 
-  B17024 <-structure(f(),
-                     class = c("B17024", acs_geography)
-  )
+  B23008 <- cls(table = "B23008",
+                geography = acs_geography,
+                lt6 = lt6,
+                other_lt6 = c("004", "005", "006", "010", "013"),
+                pth = pth)
 
-  list(tbls = list(B17024 = B17024, 
-                   B23008 = B23008),
-       raw_pth = raw_pth)
+  B17024 <- cls(table = "B17024",
+                geography = acs_geography,
+                lt6 = lt6,
+                other_lt6 = c("003", "004", "005", "006", "007", 
+                              "008", "009", "010"),
+                pth = pth)
+
+  list(B17024 = B17024, 
+       B23008 = B23008)
 }
 
 #' @title Process acs data
@@ -261,13 +224,22 @@ process.acs <- function(acs_year,
                         acs_county,
                         raw_pth) {
 
-  acs_tbls <- acs_tables(acs_year = acs_year,
-                         acs_state_code = acs_state_code,
-                         acs_geography = acs_geography,
-                         acs_county = acs_county,
-                         raw_pth = raw_pth)
+  get_key.census()
 
-  tbls <- do.call(dwnld.acs, acs_tbls)
-  acs_tbls <- c(acs_tbls, lapply(tbls, dm))
-  do.call(dm.demand, acs_tbls)
+  f <- function(x) {
+    x <- x %>%
+      dwnld.acs() %>%
+      dm_acs.default() %>%
+      dm_acs() %>%
+      dm_acs_geo() 
+  }
+  
+  tbls <- acs_tables(acs_year = acs_year,
+                     acs_state_code = acs_state_code,
+                     acs_geography = acs_geography,
+                     acs_county = acs_county,
+                     pth = raw_pth)
+
+  tbls <- sapply(tbls, f, USE.NAMES = TRUE, simplify = FALSE)
+  do.call(dm.demand, tbls)
 }
