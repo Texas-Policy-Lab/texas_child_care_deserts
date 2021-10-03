56f7c43b6834183689c5e9eb5aa1a6be634adc97
Joli Holmes <jh111@rice.edu>
2021-10-03 16:08:17 -0500

    moved function over

--- R/frontline/dm.R
+++ R/frontline/dm.R
@@ -104,6 +104,33 @@ col.seats <- function(df){
                   seats_total = availability_total + enrollment_total)
 }
 
+attr.frontline <- function(pth) {
+  input_columns = list(`OP Number` = "character",
+                       `Infant Capacity` = "numeric",
+                       `Toddler Capacity` = "numeric",
+                       `Pre- K Capacity` = "numeric",
+                       `School Aged Capacity` = "numeric",
+                       Infant_enrollment = "numeric",
+                       Toddler_enrollment = "numeric",
+                       Preschool_enrollment = "numeric",
+                       SchoolAge_enrollment = "numeric",
+                       last_modified_at_A = "character",
+                       Date = "character",
+                       export_date = "Date"
+  )
+  
+  start_date <- "07082021"
+  max_date <- "08042021"
+  
+  name <- "frontline/export_translation_Daily_Vacancy_2021-09-07_05_12_48.csv"
+  
+  list(input_columns = input_columns,
+       start_date = start_date,
+       max_date = max_date,
+       name,
+       pth = pth)
+}
+
 #' @title Download Frontline provider data
 #' @description Frontline provider data comes from the childcare.bowtiebi.com 
 #' portal (currently, only Sadie has login)
@@ -139,6 +166,8 @@ dm.frontline <- function(df,
     col.enrollment() %>%
     col.seats()
 }
+
+
 
 #' @title Process the frontline data
 process.frontline <- function(pth) {
