b9ba2cfdc8d6a626300cb81f6154385aa63a30ea
Joli Holmes <jh111@rice.edu>
2021-10-03 17:30:05 -0500

    refactored code so it is easier to manage parameters

--- R/twc_dm.R
+++ R/twc_dm.R
@@ -1,69 +1,100 @@
-#' @title Parse TWC date
-#' @param format string. The format to use to parse the string. Default is
-#' '%m.%d.%y'.
-#' @return date
-parse_date.twc <- function(x, format = "%m.%d.%y"){
-  
-  date <- lubridate::parse_date_time(x, format)
+attr.twc <- function(pth, 
+                     name = "Weekly Closure Report 9.7.21.xlsx",
+                     sheet = "All Provider Level",
+                     na = "NA",
+                     input_columns = list(`License Number` = "numeric",
+                                          `TWC`= "character",
+                                          `TRS Flag` = "character",
+                                          `Number of Current Referrals` = "numeric",
+                                          twc_date = "Date")) {
+
+  list(pth = pth,
+       name = name,
+       sheet = sheet,
+       na = na,
+       input_columns = input_columns)
 }
 
 #' @title Data management for subsidy_provider column
 #' @description Manage subsidy status
-#' @param df
-#' @return data.frame
-col.subsidy_provider <- function(df){
+#' @param x
+#' @return object
+col.subsidy_provider <- function(x) {
   
-  df <- df %>% 
+  x$df <- x$df %>% 
     dplyr::mutate(subsidy_provider = tolower(twc) == "y") %>% 
     dplyr::select(-twc)
+
+  return(x)
 }
 
 #' @title Data management for trs_provider column
 #' @description Manage trs status: binary status, convert star level to num
-#' @param df
-#' @return data.frame
-col.trs_provider <- function(df){
-  
-  df <- df %>% 
+#' @param x
+#' @return object
+col.trs_provider <- function(x) {
+
+  x$df <- x$df %>%
     dplyr::mutate(trs_provider = ifelse(trs_flag == "Regular", F, T),
-                  trs_star_level = as.numeric(gsub("\\D", "", trs_flag))) %>% 
+                  trs_star_level = as.numeric(gsub("\\D", "", trs_flag))) %>%
     dplyr::select(-trs_flag)
+
+  return(x)
 }
 
-#' @title Download TWC provider data
-#' @description TWC provider data is emailed by Shay when we request
-dwnld.twc <- function(raw_pth, 
-                      name = "Weekly Closure Report 9.7.21.xlsx",
-                      sheet = "All Provider Level") {
-  
-  df <- readxl::read_xlsx(file.path(raw_pth, name), sheet = sheet, na = "NA") %>% 
-    dplyr::mutate(twc_date = parse_date.twc(name))
+#' @title Select and rename TWC columns
+#' @param x
+#' @return object
+col.twc_select <- function(x) {
+
+  x$df <- x$df %>%
+    test_input(x$input_columns) %>%
+    dplyr::select_all(~gsub(" ", "_", tolower(.))) %>%
+    dplyr::rename(operation_number = license_number,
+                  n_subsidy_kids = number_of_current_referrals)
+
+  return(x)
 }
 
 #' @title Data management for TWC provider data
 #' @description Manage TWC data, specifically TRS status and level, subsidy status, and
 #' number of subsidy kids served
-dm.twc <- function(df,
-                   input_columns = list(`License Number` = "numeric",
-                                        `TWC`= "character",
-                                        `TRS Flag` = "character",
-                                        `Number of Current Referrals` = "numeric",
-                                        twc_date = "Date")){
-  
-  df <- df %>% 
-    test_input(input_columns) %>% 
-    dplyr::select_all(~gsub(" ", "_", tolower(.))) %>%    
-    dplyr::rename(operation_number = license_number,
-                  n_subsidy_kids = number_of_current_referrals) %>% 
-    col.operation_number() %>% 
-    col.trs_provider() %>% 
+dm.twc <- function(x) {
+
+  x <- x %>%
+    col.twc_select() %>%
+    col.operation_number() %>%
+    col.trs_provider() %>%
     col.subsidy_provider()
+
+  return(x$df)
+}
+
+#' @title Parse TWC date
+#' @param format string. The format to use to parse the string. Default is
+#' '%m.%d.%y'.
+#' @return date
+parse_date.twc <- function(x, date_format = "%m.%d.%y"){
   
+  lubridate::parse_date_time(x, date_format)
+}
+
+#' @title Download TWC provider data
+#' @description TWC provider data is emailed by Shay when we request
+dwnld.twc <- function(x) {
+
+  x$df <- readxl::read_xlsx(file.path(x$pth, x$name), 
+                          sheet = x$sheet, 
+                          na = x$na) %>% 
+    dplyr::mutate(twc_date = parse_date.twc(x$name))
+
+  return(x)
 }
 
 #' @title Process the TWC data
-process.twc <- function(raw_pth) {
-  
-  df <- dwnld.twc(raw_pth) %>% 
+process.twc <- function(pth) {
+
+  attr.twc(pth) %>%
+    dwnld.twc() %>%
     dm.twc()
 }
